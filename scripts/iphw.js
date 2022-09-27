24bebd32c32a3c7a8b00c9fea8f05c2fee06887a
sm <s1311132009@nutc.edu.tw>
2022-09-27 10:59:34 +0800

    05

--- scripts/iphw.js
+++ scripts/iphw.js
@@ -15,8 +15,8 @@ function loadImage() {
 }
 userImage.addEventListener("change", readImage);
 fileReader.addEventListener("load", loadImage);
-function imageInverse(filter="inverse") {
-    let filterResult = document.getElementById("invertResult");
+function imageInverse(filter) {
+    let filterResult = document.getElementById("filterResult");
     filterResult.width = originalImage.width;
     filterResult.height = originalImage.height;
     const inverse2D = filterResult.getContext("2d", {willReadFrequently:!0});
@@ -24,25 +24,31 @@ function imageInverse(filter="inverse") {
     filterResult = inverse2D.getImageData(0, 0, filterResult.width, filterResult.height);
     for (let a = 0; a < filterResult.data.length; a += 4) {
         switch (filter) {
-            case "inverse":
+            case "inverse": {
                 filterResult.data[a] = 255 - filterResult.data[a];
                 filterResult.data[a + 1] = 255 - filterResult.data[a + 1];
                 filterResult.data[a + 2] = 255 - filterResult.data[a + 2];
                 break;
-            case "grayscale":
+            }
+            case "grayscale": {
                 const luminance = 0.2126 * filterResult.data[a] + 0.7152 * filterResult.data[a + 1] + 0.0722 * filterResult.data[a + 2];
                 filterResult.data[a] = luminance;
-                filterResult.data[a + 1] =luminance;
+                filterResult.data[a + 1] = luminance;
                 filterResult.data[a + 2] = luminance;
                 break;
-            case "sepia":
-                const r = filterResult.data[a];
-                const g = filterResult.data[a + 1];
-                const b = filterResult.data[a + 2];
-                filterResult.data[a] = r * 0.393 + g * 0.769 + b * 0.189;
-                filterResult.data[a + 1] = r * 0.349 + g * 0.686 + b * 0.168;
-                filterResult.data[a + 2] = r * 0.272 + g * 0.534 + b * 0.131;
+            }
+            case "sepia": {
+                const red = filterResult.data[a];
+                const green = filterResult.data[a + 1];
+                const blue = filterResult.data[a + 2];
+                filterResult.data[a] = red * 0.393 + green * 0.769 + blue * 0.189;
+                filterResult.data[a + 1] = red * 0.349 + green * 0.686 + blue * 0.168;
+                filterResult.data[a + 2] = red * 0.272 + green * 0.534 + blue * 0.131;
                 break;
+            }
+            default: {
+                throw new Error(`Unknown filter: ${filter}`);
+            }
         }
     }
     
